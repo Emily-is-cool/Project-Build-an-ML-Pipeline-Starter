6613464ff999e09230714e70c5286fbe77a746fe
Emily-is-cool <ebrune@gmail.com>
2025-11-06 08:32:31 -0800

    random forest updates

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 import wandb
 import hydra
 from omegaconf import DictConfig
+from hydra.utils import to_absolute_path
+
 
 _steps = [
     "download",
@@ -66,16 +68,32 @@ def go(config: DictConfig):
 
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                to_absolute_path("src/data_check"),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
+
+        
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                }
+            )
+            
 
         if "train_random_forest" in active_steps:
 
@@ -86,12 +104,25 @@ def go(config: DictConfig):
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
+            _ = mlflow.run(
+                to_absolute_path("src/train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
 
             ##################
             # Implement here #
             ##################
 
-            pass
+            
 
         if "test_regression_model" in active_steps:
 
